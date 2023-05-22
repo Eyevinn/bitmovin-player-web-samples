4a18290a07ea5fdf1bfb7d1782dd2025c0e0a00a
Kun Wu <kun.wu@eyevinn.se>
2023-05-22 15:18:14 +0200

    refactor: use source stream generatef from srt2hls toolkit

--- typescript/index.ts
+++ typescript/index.ts
@@ -30,6 +30,107 @@ Player.addModule(SubtitlesCEA608Module);
 Player.addModule(StyleModule);
 Player.addModule(EngineNative);
 
+class Timestamp {
+  fps: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+  frameNr: number;
+  dropFrames: boolean;
+
+  constructor(fps: number, hours: number, minutes: number, seconds: number, frameNr: number, dropFrames: boolean) {
+      if (fps != 30 && fps != 60) {
+          throw new Error(`Unsupported fps: ${fps}`);
+      }
+      if (!dropFrames) {
+          throw new Error("Only drop frames is supported");
+      }
+      if (seconds == 0 && (minutes % 10 != 0) && frameNr < 2*fps/30) {
+          throw new Error(`Invalid timestamp: ${hours}:${minutes}:${seconds}:${frameNr}`);
+      }
+      if (hours > 23 || minutes > 59 || seconds > 59 || frameNr >= fps) {
+          throw new Error(`Invalid timestamp: ${hours}:${minutes}:${seconds}:${frameNr}`);
+      }
+      if (hours < 0 || minutes < 0 || seconds < 0 || frameNr < 0) {
+          throw new Error(`Invalid timestamp: ${hours}:${minutes}:${seconds}:${frameNr}`);
+      }
+      this.fps = fps;
+      this.hours = hours;
+      this.minutes = minutes;
+      this.seconds = seconds;
+      this.frameNr = frameNr;
+      this.dropFrames = dropFrames;
+  };
+
+  toString(): string {
+    const hours = this.hours.toString().padStart(2, '0');
+    const minutes = this.minutes.toString().padStart(2, '0');
+    const seconds = this.seconds.toString().padStart(2, '0');
+    let frameNr = this.frameNr.toString().padStart(2, '0');
+
+    switch (this.fps) {
+      case 30:
+        return `${hours}.${minutes}:${seconds}.${frameNr}`;
+      case 60:
+        frameNr = Math.floor(this.frameNr / 2).toString().padStart(2, '0');
+        return `${hours}.${minutes}:${seconds}:${frameNr}.${1 + this.frameNr % 2}`;
+      default:
+        throw new Error(`Unsupported fps: ${this.fps}`);
+    }
+  }
+}
+
+function NewTimestampFromMS(ms: number, fps: number, dropFrames: boolean): Timestamp {
+  if (!dropFrames) {
+    throw new Error("Only drop frames is supported");
+  }
+  const frameNr = Math.floor(Math.round(ms * fps / 1001)) + 1; // Show next frame on screen
+  return NewTimestampFromFrameNr(frameNr, fps, dropFrames);
+};
+
+function NewTimestampFromFrameNr(frameNr: number, fps: number, dropFrames: boolean): Timestamp {
+  if (!dropFrames) {
+      throw new Error("Only drop frames is supported");
+  }
+  let nrDrop = Math.floor(2 * fps / 30);
+  let framesPer10Min = (10 * 60 * fps - 9 * nrDrop);
+  let nr10MinutesStarted = Math.floor(frameNr / framesPer10Min);
+  let hours = Math.floor(nr10MinutesStarted / 6);
+  let nr10Minutes = nr10MinutesStarted - hours * 6;
+  let nr10MinuteFrames = nr10MinutesStarted * framesPer10Min;
+  let restFrameNr = frameNr - nr10MinuteFrames;
+  let maxFramesFirstMinute = 60 * fps;
+  if (restFrameNr < maxFramesFirstMinute) {
+      let seconds = Math.floor(restFrameNr / fps);
+      restFrameNr = restFrameNr - seconds * fps;
+      return new Timestamp(fps, hours, nr10Minutes * 10, seconds, restFrameNr, dropFrames);
+  }
+  let minutes = nr10Minutes * 10;
+  let nrExtraMinutes = 0;
+  if (restFrameNr < maxFramesFirstMinute) {
+      let seconds = Math.floor(restFrameNr / fps);
+      restFrameNr = restFrameNr - seconds * fps;
+      return new Timestamp(fps, hours, minutes, seconds, restFrameNr, dropFrames);
+  }
+  restFrameNr -= maxFramesFirstMinute;
+  minutes++;
+  nrExtraMinutes = Math.floor(restFrameNr / (60 * fps - nrDrop));
+  restFrameNr -= nrExtraMinutes * (60 * fps - nrDrop);
+  minutes += nrExtraMinutes;
+  if (restFrameNr < fps - nrDrop) {
+      // first second
+      let seconds = 0;
+      let frameNr = restFrameNr + nrDrop;
+      return new Timestamp(fps, hours, minutes, seconds, frameNr, dropFrames);
+  }
+  restFrameNr -= fps - nrDrop;
+  let seconds = 1;
+  let extraSeconds = Math.floor(restFrameNr / fps);
+  restFrameNr -= extraSeconds * fps;
+  seconds += extraSeconds;
+  return new Timestamp(fps, hours, minutes, seconds, restFrameNr, dropFrames);
+}
+
 const conf: PlayerConfig = {
   key: "YOUR-KEY-HERE",
   logs: {
@@ -46,7 +147,7 @@ const player = new Player(document.getElementById("player"), conf);
 const uiManager = UIFactory.buildDefaultUI(player);
 
 const source: SourceConfig = {
-  hls: "http://localhost:8090/index.m3u8",
+  hls: "http://localhost:8001/index.m3u8",
 };
 
 player.on(PlayerEvent.Paused, () => {
@@ -58,38 +159,39 @@ const formatTime = (time: number) => {
   return timeFormatted;
 }
 
-let program_data_time_minus_playback_time = 0;  // in seconds
+const FrameRateFps = 60;
+const CurrentTimeDisplay = document.getElementById('timeStamp');
+let baseProgramDateTime = 0;  // in seconds
 
 player.on(PlayerEvent.SegmentPlayback, (event ) => {
   console.log("PlayTime: " + formatTime(player.getCurrentTime()) + " SegmentPlayback: " + JSON.stringify(event));
   const segmentPlaybackEvent = event as SegmentPlaybackEvent;
-  const playbackTime = segmentPlaybackEvent.playbackTime;  // in seconds
+  const playbackTime = segmentPlaybackEvent.playbackTime;  // in seconds(float)
   const dateTime = segmentPlaybackEvent.dateTime;
   if (dateTime) {
-    const date = new Date(dateTime);
-    program_data_time_minus_playback_time = date.getTime() / 1000 - playbackTime;
+    const date = new Date(dateTime);  // in milliseconds
+    baseProgramDateTime = date.getTime() / 1000 - playbackTime;
   }
 });
 
 player.on(PlayerEvent.SourceLoaded, () => {
   console.log("SourceLoaded!");
-  const currentTimeDisplay = document.getElementById('timeCode');
-  const frameRateFps = 60;
-
-  setInterval(() => {
-    const playtime = player.getCurrentTime(); // in seconds (float)
-    const currentProgramTime = playtime + program_data_time_minus_playback_time; // in seconds (float) 
-    const timeInMinute = currentProgramTime % 60; // crop to last started minute (float)
-    const currentSecond = Math.floor(timeInMinute);
-    const currentFraction = timeInMinute - currentSecond; // in seconds
-    const currentFrame =  Math.floor(currentFraction * frameRateFps)
-    const secondAndFrame = currentSecond.toString().padStart(2, '0') + ":" + 
-                           currentFrame.toString().padStart(2, '0');
-
-    const timeInMinuteFormatted = formatTime(timeInMinute);
-
-    currentTimeDisplay.innerHTML = timeInMinuteFormatted + " <br/> Second:Frame: " + secondAndFrame;
-  }, 5);
 });
 
+// This function is called every time the browser is ready to render a new frame (60 fps).
+function updateFrameNum(t: DOMHighResTimeStamp) {
+  const playtime = player.getCurrentTime(); // in seconds (float)
+  const currentProgramTime = playtime + baseProgramDateTime; // in seconds (float) 
+  const currentMilliSecond = Math.floor(currentProgramTime * 1000);
+
+  const timestamp = NewTimestampFromMS(currentMilliSecond, FrameRateFps, true);
+  CurrentTimeDisplay.innerHTML = timestamp.toString();
+
+  // To be called recursively
+  requestAnimationFrame(updateFrameNum);
+}
+
 player.load(source);
+
+// Start the updateFrameNum function
+requestAnimationFrame(updateFrameNum);
